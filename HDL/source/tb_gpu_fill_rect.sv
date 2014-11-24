6e83ccd8482a916a0e5425048f165ce72a16ed70
Mentor Graphics <mg103@ecegrid-thin1.ecn.purdue.edu>
2014-11-24 01:01:22 -0500

    Make include paths relative and fix fill testbench

--- HDL/source/tb_gpu_fill_rect.sv
+++ HDL/source/tb_gpu_fill_rect.sv
@@ -1,5 +1,5 @@
 `timescale 1ns / 10 ps
-`include "/home/ecegrid/a/mg115/ece337/Quicksilver/HDL/source/gpu_definitions.vh"
+`include "source/gpu_definitions.vh"
 
 module tb_gpu_fill_rect();
   
@@ -18,16 +18,11 @@ module tb_gpu_fill_rect();
   reg [`HEIGHT_BITS-1:0] tb_y_o;
   reg tb_done;
   reg tb_busy;
-  reg [`CHANNEL_BITS-1:0] tb_r, tb_r_o;
-  reg [`CHANNEL_BITS-1:0] tb_g, tb_g_o;
-  reg [`CHANNEL_BITS-1:0] tb_b, tb_b_o;
   integer File; 
   
   gpu_fill_rect fill(.clk(tb_clk), .n_rst(tb_n_rst), .x1_i(tb_x1_i), .y1_i(tb_y1_i),
                       .x2_i(tb_x2_i), .y2_i(tb_y2_i), .start_i(tb_start_i),
                       .x_o(tb_x_o), .y_o(tb_y_o),
-                      .r_i(tb_r), .g_i(tb_g), .b_i(tb_b), 
-                      .r_o(tb_r_o), .g_o(tb_g_o), .b_o(tb_b_o),
                       .done_o(tb_done),
                       .busy_o(tb_busy));
          
@@ -47,8 +42,8 @@ module tb_gpu_fill_rect();
       begin
         if (File)
           begin
-            $fdisplay(File, "%d,%d,%d,%d,%d", tb_x_o, tb_y_o, tb_r_o, tb_g_o, tb_b_o);
-            $display("%d,%d,%d,%d,%d", tb_x_o, tb_y_o, tb_r, tb_g, tb_b);
+            $fdisplay(File, "%d,%d", tb_x_o, tb_y_o);
+            $display("%d,%d", tb_x_o, tb_y_o);
           end
       end
   end
@@ -62,9 +57,6 @@ module tb_gpu_fill_rect();
       $display("File opened");
     tb_n_rst = 1'b1;
     tb_n_rst = 1'b0;
-    tb_r = `CHANNEL_BITS'd50;
-    tb_g = `CHANNEL_BITS'd40;
-    tb_b = `CHANNEL_BITS'd80;
     tb_start_i = 0;
     #(CLK_PERIOD);
     tb_n_rst = 1'b1;
