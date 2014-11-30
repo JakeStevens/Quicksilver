83ffc5ad518060830b55984505ed4ab85ecd869c
manikvsin <singhalmanikv@gmail.com>
2014-11-30 16:40:57 -0500

    pre draft for the filled circle block

---
+++ HDL/source/filledcircle.sv
@@ -0,0 +1,356 @@
+`include "/home/ecegrid/a/mg118/ece337/Quicksilver/HDL/source/gpu_definitions.vh"
+module filledcircle
+(
+  input wire clk,
+  input wire n_rst,
+  input wire [`WIDTH_BITS - 1:0] xC,
+  input wire [`HEIGHT_BITS - 1:0] yC,
+  input wire [`WIDTH_BITS -1:0] rad,
+  input wire [`CHANNEL_BITS - 1:0] r_i,
+  input wire [`CHANNEL_BITS - 1:0] g_i,
+  input wire [`CHANNEL_BITS - 1:0] b_i,
+  input wire start,
+  output wire done,
+  output wire busy,
+  output wire [`WIDTH_BITS - 1:0] X,
+  output wire [`HEIGHT_BITS - 1:0] Y,
+  output wire [`CHANNEL_BITS-1:0] r_o,
+  output wire [`CHANNEL_BITS-1:0] g_o,
+  output wire [`CHANNEL_BITS-1:0] b_o
+  );
+  
+  reg signed [`WIDTH_BITS+1:0] Fcontrol;
+
+  wire start_edge;
+  reg reg_done, reg_busy;
+  reg [2:0] chordstate, nchordstate,fchange1, fchange2;
+  reg setover, nsetover; 
+  reg chordfinish;
+  reg startreg;
+  reg setpoint;
+  
+  reg [`WIDTH_BITS - 1:0] rX, rY, trX, trY, cX1, cX2;//as ry starts from radius
+  reg [`HEIGHT_BITS - 1:0] cY;
+  
+  assign r_o = r_i;
+  assign g_o = g_i;
+  assign b_o = b_i;
+  assign X = rX;
+  assign Y = rY;
+  assign busy = reg_busy;
+  assign done = reg_done;
+  
+  rise_edge_detect rise(.clk(clk),.n_rst(n_rst),.data_i(start),.rising_edge_found(start_edge));
+
+  //typedef enum bit[1:0] {NULL, START, CHORD} stateType;
+  //stateType state,nextstate
+  
+  //reg startreg;
+  
+  
+        
+  always @ (posedge clk, negedge n_rst)
+  begin: main_register
+		if (!n_rst)
+			begin
+				reg_done <= 1'b0;
+				reg_busy <= 1'b0;
+			end
+		else if (start_edge)
+			begin
+				Fcontrol <= (1 - (rad));
+				reg_busy <= 1'b1;
+        reg_done <= 1'b0;
+        trY <= rad;
+        trX <= '0;
+				startreg <= 1'b1;
+			end		
+		else if (start == 1'b1 && start_edge == 1'b0 && reg_busy == 1'b1 && setover == 1'b1)
+			begin
+				if ((trX >= trY))
+      		begin
+            reg_done <= 1'b1;
+            reg_busy <= 1'b0;
+          end
+        else
+          begin
+            if ((Fcontrol < 0))
+              begin
+                Fcontrol <= Fcontrol + (trX << 1) + 3;
+              end
+            else
+              begin
+                Fcontrol <= Fcontrol + ((trX - trY) << 1) + 5;
+                trY <= trY - 1;
+              end
+            trX <= trX + 1;                 
+          end
+				startreg <= 1'b1;
+      end
+		else
+			begin
+				reg_done <= 1'b0;
+				startreg <= 1'b0;
+			end
+	end
+
+
+	// create the statemachine which outputs the set of points for each chord into the chorddraw
+
+	
+
+	always @ (posedge clk, negedge n_rst)
+	begin
+		if (!n_rst)
+			begin
+				setover <= 1'b0;
+				chordstate <= 1'b0;
+			end
+		else
+			begin
+				setover <= nsetover;
+				chordstate <= nchordstate;
+			end
+	end
+
+	
+
+	always @ (startreg, chordstate, setover, chordfinish)
+	begin
+	  //nchordstate = (startreg) ? 3'b001 : (chordfinish && (chordstate == 3'b100)) ?  '0 : (!chordfinish) ? chordstate : chordstate + 1
+		nchordstate = chordstate;
+		nsetover = setover;
+		case (chordstate)
+			3'b000:
+				begin
+					if (startreg)
+						begin
+							nchordstate = 3'b001;
+							nsetover = 1'b0;
+							//setpoint = 1'b1;
+						end
+					else 
+					 begin
+					   nsetover = 1'b0;
+					   //setpoint = 1'b0;
+					 end  
+				end
+			3'b001:
+				begin
+					if(chordfinish)
+						begin
+							nchordstate = 3'b010;
+							nsetover = 1'b0;
+							//setpoint = 1'b1;
+						end
+					else 
+					 begin
+					   nsetover = 1'b0;
+					   //setpoint = 1'b0;
+					 end
+				end
+			3'b010:
+				begin
+					if(chordfinish)
+						begin
+							nchordstate = 3'b011;
+							nsetover = 1'b0;
+							//setpoint = 1'b1;
+						end
+					else
+					  begin
+					   nsetover = 1'b0;
+					   //setpoint = 1'b0;
+					  end
+				end
+			3'b011:
+				begin
+					if(chordfinish)
+						begin
+							nchordstate = 3'b100;
+							nsetover = 1'b0;
+							//setpoint = 1'b1;
+						end
+					else
+					  begin
+					   nsetover = 1'b0;
+					   //setpoint = 1'b0;
+					  end
+				end
+			3'b100:
+				begin
+					if(chordfinish)
+						begin
+							nchordstate = 3'b000;
+							nsetover = 1'b1;
+							//setpoint = 1'b1;
+						end
+					else
+					  begin
+					   nsetover = 1'b0;
+					   //setpoint = 1'b0;
+					  end
+				end
+			3'b101:
+				begin
+					nchordstate = 3'b000;
+				end
+			3'b110:
+				begin
+					nchordstate = 3'b000;
+				end
+			3'b111:
+				begin
+					nchordstate = 3'b000;
+				end
+		endcase
+	end
+
+
+//determine the points outputted at each chordstate
+ always_comb
+	begin
+		cX1 = `WIDTH_BITS'd`WIDTH;
+		cX2 = `WIDTH_BITS'd`WIDTH;
+		cY = `WIDTH_BITS'd`WIDTH;
+		case (chordstate)
+			3'b000:
+			begin
+				cX1 = `WIDTH_BITS'd`WIDTH;
+				cX2 = `WIDTH_BITS'd`WIDTH;
+				cY = `HEIGHT_BITS'd`WIDTH;
+			end
+			3'b001:
+				begin
+				cX1 = xC - trX;
+				cX2 = xC + trX;
+				cY = yC + trY;
+				end
+			3'b010:
+				begin
+				cX1 = xC - trY;
+				cX2 = xC + trY;
+				cY = yC + trX;
+				end
+			3'b011:
+				begin
+				cX1 = xC - trY;
+				cX2 = xC + trY;
+				cY = yC - trX;
+				end
+			3'b100:
+				begin
+				cX1 = xC - trX;
+				cX2 = xC + trX;
+				cY = yC - trY;
+				end	
+		endcase
+	end
+
+
+//
+always_ff @ (posedge clk, negedge n_rst)
+  begin
+    if (!n_rst)
+      begin
+        fchange1 <= '0;
+        fchange2 <= '0;
+      end
+    else
+      begin
+        fchange1 <= nchordstate;
+        fchange2 <= fchange1;
+      end
+  end
+  
+  assign setpoint = (fchange2 != fchange1);
+
+
+
+
+
+//statemachine to counte from one endpoint to the other endpoint of the chord
+	always @ (posedge clk, negedge n_rst)
+	begin
+		if (!n_rst)
+			begin
+			rX <= `WIDTH_BITS'd`WIDTH;
+			rY <= `HEIGHT_BITS'd`WIDTH;
+			chordfinish <= 1'b0;
+			end
+		else if (chordstate == 3'b000 || chordstate == 3'b101 || chordstate == 3'b110 || chordstate == 3'b111)
+			begin
+				rX <= `WIDTH_BITS'd`WIDTH;
+			  rY <= `HEIGHT_BITS'd`WIDTH;
+				chordfinish <= 1'b1;
+			end
+		else
+			begin
+			//rY <= cY;
+			if (setpoint)
+				begin
+					rX <= cX1;
+					rY <= cY;
+					chordfinish <= 1'b0;
+				end
+			else
+				begin
+					if (rX == cX2)
+						begin
+						  rX <= rX;
+						  rY <= cY;
+							chordfinish <= 1'b1;
+						end
+					else
+						begin
+							rX <= rX + 1;
+							rY <= cY;
+							chordfinish <= 1'b0;
+						end
+				end
+			end
+	end
+
+
+endmodule
+
+
+										
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+      
+      
+      
+      
+              
+              
+            
+        
+    
+    
+          
+                
+                
+        
+  
+  
+
--- HDL/source/tb_gpu.sv
+++ HDL/source/tb_gpu.sv
@@ -13,16 +13,19 @@ module tb_gpu();
   reg tb_pSel;
   reg tb_pEnable;
   reg tb_pWrite;
-  reg [`WIDTH_BITS-1:0] tb_x;
-  reg [`HEIGHT_BITS-1:0] tb_y;
-  reg [`CHANNEL_BITS-1:0] tb_r, tb_g, tb_b;
-  integer File;
+  reg tb_CE1_o, tb_CE0_o, tb_LB_o, tb_R_W_o, tb_UB_o, tb_ZZ_o, tb_SEM_o, tb_OE_o;
+  reg [3*(`CHANNEL_BITS) - 1:0] tb_rgbdataout_o;
+  reg [`WIDTH_BITS + `HEIGHT_BITS:0] tb_adddataout_o;
+  reg tb_buff_sel;
+  integer File1, File2;
   
   
   gpu DUT(.clk(tb_clk), .n_rst(tb_n_rst), .pAddr_i(tb_pAddr), .pDataWrite_i(tb_pDataWrite),
               .pSel_i(tb_pSel), .pEnable_i(tb_pEnable), .pWrite_i(tb_pWrite),
-              .x_o(tb_x), .y_o(tb_y), .r_o(tb_r), .g_o(tb_g), .b_o(tb_b),
-              .data_avail(tb_busy));
+              .CE1_o(tb_CE1_o), .CE0_o(tb_CE0_o), .LB_o(tb_LB_o), .R_W_o(tb_R_W_o),
+              .UB_o(tb_UB_o), .ZZ_o(tb_ZZ_o), .SEM_o(tb_SEM_o), .OE_o(tb_OE_o),
+              .rgbdataout_o(tb_rgbdataout_o), .adddataout_o(tb_adddataout_o),
+	      .buffer_select_o(tb_buff_sel));
               
     always
     begin
@@ -35,23 +38,27 @@ module tb_gpu();
     always
     begin
       @ (posedge tb_clk);
-      if(tb_busy == 1'b1)
+      if(tb_R_W_o == 1'b0)
         begin
-          if (File)
+          if (File1 && File2)
             begin
-              $fdisplay(File, "%d,%d,%d,%d,%d", tb_x, tb_y, tb_r, tb_g, tb_b);
-              $display("%d,%d,%d,%d,%d", tb_x, tb_y, tb_r, tb_g, tb_b);
+              if (tb_buff_sel == 1'b0)
+	        $fdisplay(File1, "%d,%d", tb_adddataout_o, tb_rgbdataout_o);
+              else
+		$fdisplay(File2, "%d,%d", tb_adddataout_o, tb_rgbdataout_o);
+	      $display("%d,%d", tb_adddataout_o, tb_rgbdataout_o);
             end
           end
     end
     
     initial
     begin
-      File = $fopen("/home/ecegrid/a/mg115/ece337/Quicksilver/drivers/tb_output.txt");
-        if (!File)
-          $display("file not opened");
+      File1 = $fopen("tb_output1.txt");
+      File2 = $fopen("tb_output2.txt");
+        if (!File1 || !File2)
+          $display("A file did not open");
         else
-          $display("File opened");
+          $display("Both files opened");
       tb_n_rst = 1'b1;
       tb_n_rst = 1'b0;
       #(CLK_PERIOD);
@@ -66,7 +73,7 @@ module tb_gpu();
       
       @(posedge tb_clk);
       
-      tb_pDataWrite = 32'h10000000;
+      tb_pDataWrite = 32'h300000c8;
       tb_pSel = 1'b1;
       tb_pEnable = 0'b1;
       tb_pWrite = 1'b1;
@@ -75,7 +82,7 @@ module tb_gpu();
       tb_pEnable = 1'b0;
       tb_pWrite = 1'b1;
       #(CLK_PERIOD);
-      tb_pDataWrite = 32'h20019064;
+      tb_pDataWrite = 32'h20032140;
       tb_pSel = 1'b1;
       tb_pEnable = 0'b1;
       tb_pWrite = 1'b1;
@@ -84,196 +91,7 @@ module tb_gpu();
       tb_pEnable = 1'b0;
       tb_pWrite = 1'b1;
       #(CLK_PERIOD);
-      tb_pDataWrite = 32'h40ffffff;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h10000000;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h20019032;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h40ffffff;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h10000000;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h200190c8;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h40ffffff;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h10000000;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h2001912c;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h40ffffff;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h10000000;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h200190fa;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h40ffffff;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h10000000;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h20019096;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h40ffffff;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h10000000;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h20032064;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h40ffffff;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h10000000;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h2004b064;
-      tb_pSel = 1'b1;
-      tb_pEnable = 0'b1;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pSel = 1'b1;
-      tb_pEnable = 1'b0;
-      tb_pWrite = 1'b1;
-      #(CLK_PERIOD);
-      tb_pDataWrite = 32'h40ffffff;
+      tb_pDataWrite = 32'h70ffffff;
       tb_pSel = 1'b1;
       tb_pEnable = 0'b1;
       tb_pWrite = 1'b1;
@@ -286,8 +104,9 @@ module tb_gpu();
       
       //Update this based on how many items are drawn
       //Basically, everything must be drawn before you close
-      #(CLK_PERIOD * 1000);
-      $fclose(File);
+      #(CLK_PERIOD * 10000);
+      $fclose(File1);
+      $fclose(File2);
       $display("File closed");
     end
   
